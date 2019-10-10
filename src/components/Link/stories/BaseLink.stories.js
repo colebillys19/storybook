18b934cc4521feb999d096d4966de5f246c6c721
Kwame <kamosah7@gmail.com>
2019-10-10 11:23:28 -0700

    adding ghost button link knobs

--- src/components/Link/stories/BaseLink.stories.js
+++ src/components/Link/stories/BaseLink.stories.js
@@ -1,34 +1,50 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, text, boolean } from '@storybook/addon-knobs/react';
+import {
+  withKnobs,
+  text,
+  boolean,
+  object,
+} from '@storybook/addon-knobs/react';
 
 import iconDictionary from '../../../shared/iconDictionary';
 
 import BaseLink from '../BaseLink';
-
-const linkProps = {
-  iconName: 'star',
-  iconSize: 'small',
-  label: 'Base Link Label',
-  path: 'baseLinkPath',
-  replace: true,
-};
-
 
 storiesOf('Links|BaseLink', module)
   .addDecorator((story) => (
     <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
   ))
   .addDecorator(withKnobs)
-  .add('default', () => (
-    <BaseLink label={linkProps.label} path={linkProps.path} replace />
-  ))
-  .add('icon', () => (
-    <BaseLink
-      Icon={iconDictionary(text('iconName', linkProps.iconName), text('iconSize', linkProps.iconSize))}
-      label={text('label', linkProps.label)}
-      path={text('path', linkProps.path)}
-      replace={boolean('replace', true)}
-    />
-  ));
+  .add('default', () => {
+    const label = text('label', 'Base Link Label');
+    const path = text('path', 'baseLinkPath');
+    const replace = boolean('replace', true);
+
+    return (
+      <BaseLink
+        label={label}
+        path={path}
+        replace={replace}
+      />
+    );
+  })
+  .add('icon', () => {
+    const label = text('label', 'Base Link Label', 'Link Props');
+    const path = text('path', 'baseLinkPath', 'Link Props');
+    const replace = boolean('replace', true, 'Link Props');
+    const name = text('name', 'star', 'Icon Props');
+    const size = text('size', 'small', 'Icon Props');
+    const style = text('style', '', 'Icon Props');
+    const options = object('options', { color: 'inherit' }, 'Icon Props');
+
+    return (
+      <BaseLink
+        Icon={iconDictionary(name, size, style, options)}
+        label={label}
+        path={path}
+        replace={replace}
+      />
+    );
+  });
--- src/components/Link/stories/GhostLink.stories.js
+++ src/components/Link/stories/GhostLink.stories.js
@@ -1,33 +1,49 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, text, boolean } from '@storybook/addon-knobs/react';
+import {
+  withKnobs,
+  text,
+  boolean,
+  object,
+} from '@storybook/addon-knobs/react';
 
 import iconDictionary from '../../../shared/iconDictionary';
 
 import GhostButtonLink from '../GhostButtonLink';
-
-const linkProps = {
-  iconName: 'star',
-  iconSize: 'small',
-  label: 'Ghost Button Link Label',
-  path: 'ghostButtonLinkPath',
-  replace: true,
-};
 
 storiesOf('Links|GhostButtonLink', module)
   .addDecorator(withKnobs)
   .addDecorator((story) => (
     <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
   ))
-  .add('default', () => (
-    <GhostButtonLink label={linkProps.label} path={linkProps.path} replace />
-  ))
-  .add('icon', () => (
-    <GhostButtonLink
-      Icon={iconDictionary(text('iconName', linkProps.iconName), text('iconSize', linkProps.iconSize))}
-      label={text('label', linkProps.label)}
-      path={text('path', linkProps.path)}
-      replace={boolean('replace', true)}
-    />
-  ));
+  .add('default', () => {
+    const label = text('label', 'Base Link Label');
+    const path = text('path', 'baseLinkPath');
+    const replace = boolean('replace', true);
+
+    return (
+      <GhostButtonLink
+        label={label}
+        path={path}
+        replace={replace}
+      />
+    );
+  })
+  .add('icon', () => {
+    const label = text('label', 'Base Link Label', 'Link Props');
+    const path = text('path', 'baseLinkPath', 'Link Props');
+    const replace = boolean('replace', true, 'Link Props');
+    const name = text('name', 'star', 'Icon Props');
+    const size = text('size', 'small', 'Icon Props');
+    const style = text('style', '', 'Icon Props');
+    const options = object('options', { color: 'inherit' }, 'Icon Props');
+    return (
+      <GhostButtonLink
+        Icon={iconDictionary(name, size, style, options)}
+        label={label}
+        path={path}
+        replace={replace}
+      />
+    );
+  });
