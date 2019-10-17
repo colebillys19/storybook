0ac80f855a9e767b541213d5084d2d5073ab95b5
Billy Cole <cole.billys@gmail.com>
2019-10-17 10:23:46 -0700

    add doc block to top of story files

--- src/components/Headings/stories/H1.stories.js
+++ src/components/Headings/stories/H1.stories.js
@@ -1,3 +1,11 @@
+/**
+ * @description component description
+ * @props
+ * prop name (prop type) prop description
+ * prop name (prop type) prop description
+ * prop name (prop type) prop description
+ */
+
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import {
--- src/components/Link/stories/GhostLink.stories.js
+++ src/components/Link/stories/GhostLink.stories.js
@@ -1,3 +1,11 @@
+/**
+ * @description component description
+ * @props
+ * prop name (prop type) prop description
+ * prop name (prop type) prop description
+ * prop name (prop type) prop description
+ */
+
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { storiesOf } from '@storybook/react';
