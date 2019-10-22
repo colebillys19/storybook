4212daccb92c6e28262f200ed9e8418542a193db
Billy Cole <cole.billys@gmail.com>
2019-10-22 13:33:15 -0700

    remove files to fix bug

--- src/components/Input/sub-components/ShowPasswordIcon.styles.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import IconButton from '@material-ui/core/IconButton';
-
-export const StyledPasswordIcon = styled(IconButton)`
-  left: 0.8rem;
-  padding: 0.8rem;
-  position: relative;
-`;
