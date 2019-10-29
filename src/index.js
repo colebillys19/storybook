f136c7eaf0a64095cf26d8ab60197439e12b53fc
Billy Cole <cole.billys@gmail.com>
2019-10-29 15:59:20 -0700

    change from dist to compiled, revert export method

--- src/index.js
+++ src/index.js
@@ -2,85 +2,24 @@ import globalStyles from '../.storybook/globalStyle';
 import * as defaultStyles from './utils/defaultStyleHelper';
 import * as testingHelpers from './utils/testingHelpers';
 
-import AlertBanner from './components/Banner/AlertBanner';
-import ErrorSuccessBanner from './components/Banner/ErrorSuccessBanner';
-import BaseButton from './components/Button/BaseButton';
-import FlatIconButton from './components/Button/FlatIconButton';
-import GhostButton from './components/Button/GhostButton';
-import IconButton from './components/Button/IconButton';
-import PrimaryButton from './components/Button/PrimaryButton';
-import SecondaryButton from './components/Button/SecondaryButton';
-import TextButton from './components/Button/TextButton';
-import TooltipButton from './components/Button/TooltipButton';
-import BaseCard from './components/Card/BaseCard';
-import Checkbox from './components/Checkbox/Checkbox';
-import ExpansionPanel from './components/ExpansionPanel/ExpansionPanel';
-import H1 from './components/Heading/H1';
-import H2 from './components/Heading/H2';
-import H3 from './components/Heading/H3';
-import TooltipIcon from './components/Icon/TooltipIcon';
-import CurrencyInput from './components/Input/CurrencyInput';
-import NumberInput from './components/Input/NumberInput';
-import PasswordInput from './components/Input/PasswordInput';
-import TextInput from './components/Input/TextInput';
-import BaseLink from './components/Link/BaseLink';
-import GhostButtonLink from './components/Link/GhostButtonLink';
-import ListItem from './components/ListItem/ListItem';
-import LoadingIndicator from './components/LoadingIndicator/LoadingIndicator';
-import SmallLoadingIndicator from './components/LoadingIndicator/SmallLoadingIndicator';
-import BasePaper from './components/Paper/BasePaper';
-import InsetPaper from './components/Paper/InsetPaper';
-import RadioButtonGroup from './components/Radio/RadioButtonGroup';
-import BaseTable from './components/Table/BaseTable';
-
 export {
-  AlertBanner,
-  BaseButton,
-  BaseCard,
-  BaseLink,
-  BasePaper,
-  BaseTable,
-  Checkbox,
-  CurrencyInput,
   defaultStyles,
-  ErrorSuccessBanner,
-  ExpansionPanel,
-  FlatIconButton,
-  GhostButton,
-  GhostButtonLink,
   globalStyles,
-  H1,
-  H2,
-  H3,
-  IconButton,
-  InsetPaper,
-  ListItem,
-  LoadingIndicator,
-  NumberInput,
-  PasswordInput,
-  PrimaryButton,
-  RadioButtonGroup,
-  SecondaryButton,
-  SmallLoadingIndicator,
   testingHelpers,
-  TextButton,
-  TextInput,
-  TooltipButton,
-  TooltipIcon,
 };
 
-// export * from './components/Banner';
-// export * from './components/Button';
-// export * from './components/Card';
-// export * from './components/Checkbox';
-// export * from './components/ExpansionPanel';
-// export * from './components/Heading';
-// export * from './components/Icon';
-// export * from './components/Input';
-// export * from './components/Link';
-// export * from './components/ListItem';
-// export * from './components/LoadingIndicator';
-// export * from './components/Paper';
-// export * from './components/Radio';
-// export * from './components/Table';
-// export * from './svg';
+export * from './components/Banner';
+export * from './components/Button';
+export * from './components/Card';
+export * from './components/Checkbox';
+export * from './components/ExpansionPanel';
+export * from './components/Heading';
+export * from './components/Icon';
+export * from './components/Input';
+export * from './components/Link';
+export * from './components/ListItem';
+export * from './components/LoadingIndicator';
+export * from './components/Paper';
+export * from './components/Radio';
+export * from './components/Table';
+export * from './svg';
