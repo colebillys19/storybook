81f8fe013301e4b0b459a43ff296575d502b5579
Kwame <kamosah7@gmail.com>
2019-11-12 12:02:54 -0800

    adding date picker tests

--- src/components/DatePicker/tests/BaseDatePicker.test.js
+++ src/components/DatePicker/tests/BaseDatePicker.test.js
@@ -1,10 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
+import moment from 'moment';
 import {
   cleanup,
-  // fireEvent,
-  // render,
+  fireEvent,
+  render,
 } from '@testing-library/react';
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
+// Needed to add classes to all DatePicker components
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
 
 import {
   checkBasicRendering,
@@ -15,11 +19,31 @@ import BaseDatePicker from '../BaseDatePicker';
 afterEach(cleanup);
 
 describe('<BaseDatePicker />', () => {
+  const initialDate = new Date().toString();
   const component = (
     <BaseDatePicker
       id="test-date-picker"
+      initialDate={initialDate}
     />
   );
+
+  it('renders a single date picker', async () => {
+    const { container } = render(component);
+    const datePickerInput = container.querySelector('input[name="test-date-picker"]');
+    expect(container.querySelector('div.SingleDatePicker_picker')).not.toBeInTheDocument();
+    fireEvent.focus(datePickerInput);
+    expect(container.querySelector('div.SingleDatePicker_picker')).toBeInTheDocument();
+  });
+
+  it('changes the date', () => {
+    const { container } = render(component);
+    const datePickerInput = container.querySelector('input[name="test-date-picker"]');
+    const today = moment(initialDate).format('L');
+    const tomorrow = moment(today).add(1, 'd').format('L');
+    expect(datePickerInput.value).toBe(today);
+    fireEvent.change(datePickerInput, { target: { value: tomorrow } });
+    expect(datePickerInput.value).toBe(tomorrow);
+  });
 
   checkBasicRendering(component);
   checkRequiredProps(component);
